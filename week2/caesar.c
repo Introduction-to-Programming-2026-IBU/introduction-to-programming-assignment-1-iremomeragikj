59953fd64a75d0abfda0dcc3ed72c78dd0743ce2
Irem Omeragikj <iremomeragic19@gmail.com>
2026-03-17 14:03:16 +0100

    Add files via upload

---
+++ week2/caesar.c
@@ -0,0 +1,60 @@
+#include <cs50.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// Function prototypes
+bool only_digits(string s);
+char rotate(char c, int n);
+
+int main(int argc, string argv[])
+{
+    if(argc != 2 || !only_digits(argv[1]))
+    {
+        printf("Usage: ./caesar key\n");
+        return 1;
+    }
+
+    int key = atoi(argv[1]) % 26;
+
+    string plaintext = get_string("plaintext:  ");
+
+    printf("ciphertext: ");
+
+    for(int i = 0; i < strlen(plaintext); i++)
+    {
+        char encrypted = rotate(plaintext[i], key);
+        printf("%c", encrypted);
+    }
+    printf("\n");
+    return 0;
+}
+
+bool only_digits(string s)
+{
+    for(int i = 0; i < strlen(s); i++)
+    {
+        if(!isdigit(s[i]))
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+char rotate(char c, int n)
+{
+    if(isupper(c))
+    {
+        return (c - 'A' + n) % 26 + 'A';
+    }
+    else if (islower(c))
+    {
+        return (c - 'a' + n) % 26 + 'a';
+    }
+    else
+    {
+         return c;
+    }
+}
